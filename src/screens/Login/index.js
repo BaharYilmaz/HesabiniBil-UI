fc3b5dc6580fcb1eb1d47f59364fdc7421fb437d
Bahar Yılmaz <baharr.yilmaz48@gmail.com>
2020-10-24 19:52:11 +0300

    login form validaiton added

--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -7,7 +7,6 @@ import {
 } from 'react-native';
 
 
-
 import { Container, Header, Content, Button, Form, Item, Input, Title, H2, H3, Text, Body, Badge } from 'native-base';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
@@ -18,35 +17,45 @@ const Login = (props) => {
     const login = useContext(AppContext);
     const { height: screenHeight } = Dimensions.get('window');
 
+    const [loginForm, setValid] = React.useState({ emailValid: true, passwordValid: true });
+
     const emailChange = (val) => {
-        login.setLoginData({ ...login.loginData, email: val });
+
+        let reg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+        if (reg.test(val) === false ) {
+            login.setLoginData({ ...login.loginData, email: val });
+            setValid({ ...loginForm, emailValid: false });
+        }
+        else {
+            login.setLoginData({ ...login.loginData, email: val });
+            setValid({ ...loginForm, emailValid: true });
+        }
     }
     const passwordChange = (val) => {
-        login.setLoginData({ ...login.loginData, password: val });
+
+        if (val.trim().length > 0) {
+            login.setLoginData({ ...login.loginData, password: val });
+            setValid({ ...loginForm, passwordValid: true });
+        }
+        else {
+            login.setLoginData({ ...login.loginData, password: val });
+            setValid({ ...loginForm, passwordValid: false });
+        }
     }
+
     const handleSubmit = () => {
 
-        // if (userForm.valid) { 
-        // } else {
-        // }
-        login.handleLogin();
+        if (login.password != null && login.email != null) {
+            login.handleLogin();
+        }
+        else {
+            setValid({ emailValid: false, passwordValid: false });
+        }
     }
-
     return (
 
         <Container >
             <Header />
-            {/* <Header>
-                    <Left>
-                        <Button transparent>
-                            <Icon name='arrow-left-thick' size={30} color="white" />
-                        </Button>
-                    </Left>
-                    <Body>
-                        <Title>Title</Title>
-                        color="#635DB7"
-                    </Body> 
-                </Header>*/}
             <Content >
                 <View style={{ flex: 1, height: screenHeight, justifyContent: 'center' }}>
                     <View style={{ alignItems: 'center' }}>
@@ -54,16 +63,24 @@ const Login = (props) => {
                     </View>
 
                     <Form style={{ margin: 20 }}>
-                        <Item style={{ margin: 20, padding: 5 }}>
-                            <Input placeholder="E-posta" onChangeText={(val) => emailChange(val)} />
-                            <Icon name='account-outline' size={30} color="gray" />
 
+                        <Item style={{ margin: 15, padding: 5 }}   >
+                            <Input placeholder="E-posta" onChangeText={(val) => emailChange(val)} />
+                            <Icon name='email-outline' size={30} color="gray" />
                         </Item>
-                        <Item style={{ margin: 20, padding: 5 }}>
+                        {loginForm.emailValid ? null :
+                            <Text style={{ margin: 15, padding: 5 ,color:'red' }}>Lütfen uygun bir mail adresi giriniz !</Text>
+                        }
+
+                        <Item style={{ margin: 15, padding: 5 }} >
                             <Input placeholder="Şifre" onChangeText={(val) => passwordChange(val)} />
                             <Icon name='lock-outline' size={30} color="gray" />
                         </Item>
-                        <Button block rounded style={{ margin: 15, padding: 5 }} onPress={handleSubmit} ><Text> Giriş Yap </Text></Button>
+                        {loginForm.passwordValid ? null :
+                            <Text error style={{ margin: 15, padding: 5 ,color:'red' }}>Lütfen şifrenizi giriniz !</Text>
+                        }
+
+                        <Button block rounded style={{ margin: 25, padding: 5}} onPress={handleSubmit} ><Text> Giriş Yap </Text></Button>
                         <View style={{ alignItems: 'center' }}>
                             <Text> Hesabın yok mu ?</Text>
                             <Button transparent block onPress={() => props.navigation.navigate('SignUp')} >
@@ -72,6 +89,7 @@ const Login = (props) => {
                         </View>
                     </Form>
                 </View>
+
             </Content>
         </Container>
     );
